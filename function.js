2d0810fca3ba96da10c2d581369907f2052de804
kalitajyotishmoy27-hash <kalitajyotishmoy27@gmail.com>
2026-09-22 17:22:34 +0530

    use function

---
+++ function.js
@@ -0,0 +1,29 @@
+function greet(name)
+{
+    console.log("Hello "+name);
+}
+
+// console.log(greet("Rahul"));
+// console.log(greet("Priya"));
+// console.log(greet("Amit"));
+
+greet("Rahul");
+greet("Priya");
+greet("Amit")
+
+
+function sum(a,b)
+{
+    return a+b;
+}
+
+console.log("Sumation of a and b is: "+sum(10,20));
+
+function introduce(name, age,course)
+{
+    console.log("Name: "+name);
+    console.log("Age: "+age);
+    console.log("Course: "+course);
+}
+
+introduce("Jyotishmoy Kalita",18,"BCA");
